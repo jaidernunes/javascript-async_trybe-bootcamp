ff4364cfeca568546b33144a0c270b24f823aec1
jaidernunes <jaidernf@gmail.com>
2022-09-12 11:48:10 -0300

    req 2 ok

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,3 @@
-
 const fetchCoins = async () => {
   const endpoint1 = 'https://api.coincap.io/v2/assets';
   const request = await fetch(endpoint1);
@@ -12,6 +11,10 @@ const makeList = async () => {
  const list = document.getElementById('coins-list');
  coinsArr.map((coin) => {
   const createLi = document.createElement('li');
-  list.innerText = `${coin.name} (${coin.symbol}): ${(coin.priceUsd).toFixed(2)}`;
- })
-}
+  createLi.innerText = `${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)}`;
+  list.appendChild(createLi);
+  console.log(createLi);
+  return undefined;
+ });
+};
+makeList();
